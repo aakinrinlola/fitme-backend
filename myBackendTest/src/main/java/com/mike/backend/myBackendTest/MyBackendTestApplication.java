057b4661ef6197e0c7f032af399af018aad2aca1
aakinrinlola <a.akinrinlola@hotmail.com>
2026-03-01 00:44:30 +0100

    Backend Erstellung

---
+++ myBackendTest/src/main/java/com/mike/backend/myBackendTest/MyBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.mike.backend.myBackendTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBackendTestApplication.class, args);
+	}
+
+}
---
+++ myBackendTest/src/test/java/com/mike/backend/myBackendTest/MyBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.backend.myBackendTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
